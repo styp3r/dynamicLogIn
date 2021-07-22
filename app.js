d769cb9cf92f178052b9b0a8e5a4b5c0083a3329
styp3r <stypertypes@gmail.com>
2021-07-22 10:58:36 +0530

    add effect on button hover by adding class with css effect

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ $(".emailInput").click(()=>{
   $(".waveAnimation1").addClass("wave1");
   $(".waveAnimation2").addClass("wave2");
 });
+
+$(".logInButton").hover(()=>{
+  $(".wave1").addClass("blueHue1");
+  $(".wave2").addClass("blueHue2");
+}, ()=>{
+  $(".wave1").removeClass("blueHue1");
+  $(".wave2").removeClass("blueHue2");
+});
